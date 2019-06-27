6501da77a9b4655ed4035367e73f9aef1c4a2b68
sds38 <Heather10!G>
2019-06-26 21:59:19 -0400

    html class

--- public/index.php
+++ public/index.php
@@ -27,9 +27,23 @@ class csv {
 
 class html
 {
-    static public function generateTable() {}
-
-
+    static public function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
         }
 class system{
     static public function printPage($page) {}
