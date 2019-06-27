18bf6908ca3782dd8088e642885118166b5f8b23
sds38 <Heather10!G>
2019-06-27 17:50:57 -0400

    create index.php, read file set up

--- public/index.php
+++ public/index.php
@@ -11,39 +11,29 @@ main::start("FL_insurance_sample.csv");
 class main
 {
 
-    static public function start($filename) {}
+    static public function start($filename) {
 
 
         $records = csv::getRecords();
         $table = html::generateTable($records);
         system::printPage($table);
+    )
     }
 
-class csv {
+class csv
+{
 
-        static public function getRecords() {}
+    static public function getRecords()
+    {
+
+        $records[] =
 
         }
-
+}
 class html
 {
     static public function generateTable($records) {
-        $count = 0;
-        foreach ($records as $record) {
-            if($count == 0) {
-                $array = $record->returnArray();
-                $fields = array_keys($array);
-                $values = array_values($array);
-                print_r($fields);
-                print_r($values);
-            } else {
-                $array = $record->returnArray();
-                $values = array_values($array);
-                print_r($values);
-            }
-            $count++;
-        }
-    }
+    echo 'swag';
         }
 class system{
     static public function printPage($page) {}
