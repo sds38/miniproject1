26773808ecc93f560e9eabe2f7d0f6ed317a8cfd
sds38 <Heather10!G>
2019-06-26 21:57:39 -0400

    Updated classes, set up localhost environment

--- public/index.php
+++ public/index.php
@@ -4,4 +4,34 @@
 
  */
 
-echo 'hello world';
+
+
+main::start("FL_insurance_sample.csv");
+
+class main
+{
+
+    static public function start($filename) {}
+
+
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+
+class csv {
+
+        static public function getRecords() {}
+
+        }
+
+class html
+{
+    static public function generateTable() {}
+
+
+        }
+class system{
+    static public function printPage($page) {}
+    print 'hello world';
+    }
