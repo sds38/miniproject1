e05652fa2d4f2a1de89db379aa7eeabcc3e5e5db
sds38 <Heather10!G>
2019-06-28 16:25:41 -0400

    html class

--- public/index.php
+++ public/index.php
@@ -20,12 +20,62 @@ class main
 
         $records = csv::getRecords($filename);
         $table = html::generateTable($records);
+        system::printTable($table)
 
     }
 }
-class csv{}
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = dataFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
 
-class html
+class dataFactory{
+        static public function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
+}
+//need to create the property, and return the array
+class data{
+       public function _construct()
+            {$record = array_combine($fieldNames,$values)}
+        }
+}
+
+//need to include html for table in foreach loop
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
+}
 
 
 ?>
